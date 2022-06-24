b872b2e7a181ac3b8cec95d1dbe8a0958fce7f4d
GimmyFox <mguzeev99@gmail.com>
2022-06-24 18:05:27 +0300

    Initial Commit

---
+++ TodoList_SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList_SUI
+//
+//  Created by Maksim Guzeev on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList_SUI/TodoList_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SUIApp.swift
+//  TodoList_SUI
+//
+//  Created by Maksim Guzeev on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
